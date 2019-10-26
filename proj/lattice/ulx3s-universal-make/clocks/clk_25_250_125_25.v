05ab379f70ec53e3dcad22509b70c0b96ae82f75
emard <vordah@gmail.com>
2019-10-26 17:45:58 +0200

    ULX3S: new clock PLL

--- proj/lattice/ulx3s-universal-make/clocks/clk_25_250_125_25.v
+++ proj/lattice/ulx3s-universal-make/clocks/clk_25_250_125_25.v
@@ -1,3 +1,7 @@
+// diamond 3.7 accepts this PLL
+// diamond 3.8-3.9 is untested
+// diamond 3.10 or higher is likely to abort with error about unable to use feedback signal
+// cause of this could be from wrong CPHASE/FPHASE parameters
 module clk_25_250_125_25
 (
     input clki, // 25 MHz, 0 deg
@@ -6,20 +10,25 @@ module clk_25_250_125_25
     output clkos2, // 25 MHz, 0 deg
     output locked
 );
-wire clkfb;
-wire clkos;
-wire clkop;
+(* FREQUENCY_PIN_CLKI="25" *)
+(* FREQUENCY_PIN_CLKOP="250" *)
+(* FREQUENCY_PIN_CLKOS="125" *)
+(* FREQUENCY_PIN_CLKOS2="25" *)
 (* ICP_CURRENT="12" *) (* LPF_RESISTOR="8" *) (* MFG_ENABLE_FILTEROPAMP="1" *) (* MFG_GMCREF_SEL="2" *)
 EHXPLLL #(
         .PLLRST_ENA("DISABLED"),
-        .STDBY_ENABLE("DISABLED"),
         .INTFB_WAKE("DISABLED"),
+        .STDBY_ENABLE("DISABLED"),
         .DPHASE_SOURCE("DISABLED"),
-        .CLKOP_FPHASE(0),
-        .CLKOP_CPHASE(0),
         .OUTDIVIDER_MUXA("DIVA"),
+        .OUTDIVIDER_MUXB("DIVB"),
+        .OUTDIVIDER_MUXC("DIVC"),
+        .OUTDIVIDER_MUXD("DIVD"),
+        .CLKI_DIV(1),
         .CLKOP_ENABLE("ENABLED"),
         .CLKOP_DIV(2),
+        .CLKOP_CPHASE(0),
+        .CLKOP_FPHASE(0),
         .CLKOS_ENABLE("ENABLED"),
         .CLKOS_DIV(4),
         .CLKOS_CPHASE(0),
@@ -28,18 +37,17 @@ EHXPLLL #(
         .CLKOS2_DIV(20),
         .CLKOS2_CPHASE(0),
         .CLKOS2_FPHASE(0),
-        .CLKFB_DIV(10),
-        .CLKI_DIV(1),
-        .FEEDBK_PATH("INT_OP")
+        .FEEDBK_PATH("CLKOP"),
+        .CLKFB_DIV(10)
     ) pll_i (
+        .RST(1'b0),
+        .STDBY(1'b0),
         .CLKI(clki),
-        .CLKFB(clkfb),
-        .CLKINTFB(clkfb),
         .CLKOP(clkop),
         .CLKOS(clkos),
         .CLKOS2(clkos2),
-        .RST(1'b0),
-        .STDBY(1'b0),
+        .CLKFB(clkop),
+        .CLKINTFB(),
         .PHASESEL0(1'b0),
         .PHASESEL1(1'b0),
         .PHASEDIR(1'b1),
@@ -49,5 +57,4 @@ EHXPLLL #(
         .ENCLKOP(1'b0),
         .LOCK(locked)
 	);
-assign clkop = clkop;
 endmodule
